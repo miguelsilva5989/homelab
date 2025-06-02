c2717509b74df07b3cfa86e26619b0bb945ad24e
Miguel Silva <miguelsilva5989@gmail.com>
2025-06-02 16:48:40 +0200

    fix matrix 02 and 03 config

---
+++ terraform/cluster/vm_architect_01.tf
@@ -0,0 +1,19 @@
+# resource "proxmox_vm_qemu" "architect_01" {
+#     name = "architect-03"
+#     desc = "CICD Ubuntu Server 24"
+#     vmid = "113"
+#     target_node = "matrix"
+
+#     agent = 1 # qemu
+
+#     clone = "ubuntu-server-24-noble" # name of the template VM to clone from
+#     cores = 2
+#     memory = 16384
+
+#     os_type = "cloud-init"
+#     ipconfig0 = "ip=10.69.5.13/16,gw=10.69.0.1"
+#     nameserver = "10.69.0.1"
+
+#     ciuser = "mike"
+#     sshkeys = file(var.ssh_public_key_path)
+# }
---
+++ terraform/cluster/vm_matrix_02.tf
@@ -0,0 +1,51 @@
+resource "proxmox_vm_qemu" "matrix_02" {
+    name = "matrix-02"
+    desc = "CICD Ubuntu Server 24"
+    vmid = "102"
+    target_node = "matrix"
+
+    agent = 1 # qemu
+
+    clone = "ubuntu-server-24-noble" # name of the template VM to clone from
+
+    cores = 2
+    cpu_type = "x86-64-v4"
+    memory = 16384
+
+    network {
+        id  = 0
+        bridge = "vmbr0"
+        model = "virtio"
+    }
+
+    scsihw = "virtio-scsi-single"
+
+    disks {
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local-zfs"
+                }
+            }
+        }
+        virtio {
+            virtio0 {
+                disk {
+                    storage = "local-zfs"
+                    size = "80G"
+                    # NOTE Enable IOThread for better disk performance in virtio-scsi-single
+                    #      and enable disk replication
+                    iothread = true
+                    replicate = true
+                }
+            }
+        }
+    }
+
+    os_type = "cloud-init"
+    ipconfig0 = "ip=10.69.5.2/16,gw=10.69.0.1"
+    nameserver = "10.69.0.1"
+
+    ciuser = "mike"
+    sshkeys = file(var.ssh_public_key_path)
+}
